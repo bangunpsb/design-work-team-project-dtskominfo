e2bf6ee9cf88b47be5a40bb8dc1ed8c8559e78ac
Bangun Pasaribu <50303571+bangunpsb@users.noreply.github.com>
2020-04-24 11:42:19 +0700

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,4 @@
+// slide click
 let slideIndex = 1;
 showDivs(slideIndex);
 
@@ -18,4 +19,38 @@ function showDivs(n) {
         x[i].style.display = "none";
     }
     x[slideIndex - 1].style.display = "block";
+}
+
+
+// typed
+let typed = new Typed('.typed', {
+    strings: ["Web Developer.", "Mobile Apps Developer.", "Desktop Apps Developer.", "UI/UX Designer.", "System Analyst."],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+});
+
+
+// modal action
+function getId() {
+    // cek value btn yg diklik
+    window.onclick = e => {
+        const targetId = e.target.value;
+        // dapatkan id modal berdasarkan value btn yg diklik
+        const modal = document.getElementById(targetId);
+        const span = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
+
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // ketika diklik diluar modal display none atau tdk ada
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
+
 }
