8729dcfd8828c1b414429eb397a115f5c1764a29
Bangun Pasaribu <50303571+bangunpsb@users.noreply.github.com>
2020-04-25 04:57:59 +0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,25 +32,46 @@ let typed = new Typed('.typed', {
 
 
 // modal action
-function getId() {
-    // cek value btn yg diklik
-    window.onclick = e => {
-        const targetId = e.target.value;
-        // dapatkan id modal berdasarkan value btn yg diklik
-        const modal = document.getElementById(targetId);
-        const span = document.getElementsByClassName("close")[0];
-        modal.style.display = "block";
+// function getId() {
+//     // cek value btn yg diklik
+//     window.onclick = e => {
+//         const targetId = e.target.value;
+//         // dapatkan id modal berdasarkan value btn yg diklik
+//         const modal = document.getElementById(targetId);
+//         const span = document.getElementsByClassName("close")[0];
+//         modal.style.display = "block";
 
-        span.onclick = function () {
+//         span.onclick = function () {
+//             modal.style.display = "none";
+//         }
+
+//         // ketika diklik diluar modal display none atau tdk ada
+//         window.onclick = function (event) {
+//             if (event.target == modal) {
+//                 modal.style.display = "none";
+//             }
+//         }
+//     }
+
+// }
+
+
+function getId(e) {
+    const modal = document.getElementById(e);
+    modal.style.display = "block";
+
+
+    const span = document.getElementsByClassName("close");
+    Array.from(span).forEach((el) => {
+        el.onclick = function () {
             modal.style.display = "none";
         }
+    });
 
-        // ketika diklik diluar modal display none atau tdk ada
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
+    // ketika diklik diluar modal display none atau tdk ada
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
     }
-
 }
